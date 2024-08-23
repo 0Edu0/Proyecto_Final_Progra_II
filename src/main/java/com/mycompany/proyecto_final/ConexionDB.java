4e8a4f4b6964c7170613f104a88b3591061eb00f
Luis Contreras <lockyalejandro5078@gmail.com>
2024-08-23 12:44:20 -0600

    Se implemento menu pero no esta funcionando correctamente

--- src/main/java/com/mycompany/proyecto_final/ConexionDB.java
+++ src/main/java/com/mycompany/proyecto_final/ConexionDB.java
@@ -20,7 +20,7 @@ public class ConexionDB {
     
     public Connection estableceConexion(){
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             conectar=DriverManager.getConnection(cadena, usuario, contrasenia);
             JOptionPane.showMessageDialog(null, "Se conecto a la base de datos");
             
--- src/main/java/com/mycompany/proyecto_final/Proyecto_Final.java
+++ src/main/java/com/mycompany/proyecto_final/Proyecto_Final.java
@@ -1,15 +1,80 @@
-
 package com.mycompany.proyecto_final;
 
+import java.util.Scanner;
 
 public class Proyecto_Final {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Haciendo Prueba hacer un commit en git");
         
-        TipoPokemon pokemonAgua = new TipoPokemon();
-        pokemonAgua.getPokemonesDeAgua();
-        
+        Proyecto_Final proyecto = new Proyecto_Final();
+        proyecto.MenuPokemon(); 
     }
+
+     public void MenuPokemon() {
+         TipoPokemon tipoPokemon = new TipoPokemon();
+         
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
+        
+        
+
+        while (!salir) {
+              System.out.println("Ingrese una opción:");
+                System.out.println("1. Ver pokemones normales");
+                System.out.println("2. Ver pokemones de Agua");
+                System.out.println("3. Ver pokemones de Fuego");
+                System.out.println("4. Ver pokemones de Planta");
+                System.out.println("5. Ver pokemones eléctricos");
+                System.out.println("6. Ver pokemones de Hielo");
+                System.out.println("7. Salir");
+
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    tipoPokemon.getPokemonesNormales();
+                    break;
+                    
+                case 2:
+                 tipoPokemon.getPokemonesDeAgua();
+                    break;
+
+                case 3:
+                   tipoPokemon.getPokemonesDeFuego();
+
+                    break;
+
+                case 4:
+                     tipoPokemon.getPokemonesDePlanta();
+                    break;
+                    
+                   case 5:
+                     tipoPokemon.getPokemonesDeElectrico();
+                    break;
+                    
+                 case 6:
+                     tipoPokemon.getPokemonesDeHielo();
+                    break;
+                    
+                   case 7:
+                   salir = true;
+                    break;
+
+                default:
+                    System.out.println("Opción no válida. Por favor intente de nuevo.");
+            }
+        }
+
+        System.out.println("Programa terminado.");
+    }
+    
+   
+
+        
+              
+
+                
+
+         
 }
