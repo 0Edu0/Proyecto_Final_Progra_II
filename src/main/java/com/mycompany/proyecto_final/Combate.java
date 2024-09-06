2ad5f931776ae3b152fad672ef47374c568ffcb1
0Edu0 <edulink.1610@gmail.com>
2024-09-05 21:55:22 -0600

    Pokemon tipo agua para combate y sistema de comabate

--- src/main/java/com/mycompany/proyecto_final/Combate.java
+++ src/main/java/com/mycompany/proyecto_final/Combate.java
@@ -1,39 +1,75 @@
-
 package com.mycompany.proyecto_final;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Combate {
+
+public class Combate { 
+    private ArrayList<PokemonCombate> equipoUsuario;
+    private ArrayList<PokemonCombate> equipoComputadora;
     
-    Entrenador entrenador;
-    Entrenador contrincante;
-    String turnoActual;
-
-    public Combate(Entrenador entrenador, Entrenador contrincante, String turnoActual) {
-        this.entrenador = entrenador;
-        this.contrincante = contrincante;
-        this.turnoActual = turnoActual;
+    public Combate(ArrayList<PokemonCombate> equipoUsuario, ArrayList<PokemonCombate> equipoComputadora) {
+        this.equipoUsuario = equipoUsuario;
+        this.equipoComputadora = equipoComputadora;
     }
+    public void iniciarCombate(Scanner scanner){
+        int turnoUsuario = 0;
+        int turnoComputadora = 0;
+        
+        while (!equipoUsuario.isEmpty() && !equipoComputadora.isEmpty()){
+            System.out.println("Tu Pokemon actual: " + equipoUsuario.get(turnoUsuario).getNombre());
+            System.out.println("Pokemon de la computadora: " + equipoComputadora.get(turnoComputadora).getGenero());
+            
+            System.out.println("Elige una habilidad para atacar: ");
+            for (int i = 0; i < equipoUsuario.get(turnoUsuario).getHabilidades().size(); i++){
+                Habilidades habilidad = equipoUsuario.get(turnoUsuario).getHabilidades().get(i);
+                System.out.println((i + 1) + " " + habilidad.getNombreHabilidad() + " - Potencia: " + habilidad.getPotencia());
+            }
+            int seleccionHabilidad = scanner.nextInt();
+            scanner.nextLine();
+            
+            if (seleccionHabilidad > 0 && seleccionHabilidad <= equipoUsuario.get(turnoUsuario).getHabilidades().size()) {
+                Habilidades habilidadSeleccionada = equipoUsuario.get(turnoUsuario).getHabilidades().get(seleccionHabilidad - 1);
+                realizarAtaque(habilidadSeleccionada, equipoComputadora.get(turnoComputadora));
+                
+                if (equipoComputadora.get(turnoComputadora).getHp_base() <= 0){
+                    System.out.println("Has derrotado a " + equipoComputadora.get(turnoComputadora).getNombre());
+                    equipoComputadora.remove(turnoComputadora);
+                
+                    if (equipoComputadora.isEmpty()){
+                        System.out.println("Has ganado el combate");
+                        return;
+                    }
+                }
+                
+            realizarAtaqueComputadora(equipoComputadora.get(turnoComputadora), equipoUsuario.get(turnoUsuario));
+            
+                if (equipoUsuario.get(turnoUsuario).getHp_base() <= 0){
+                    System.out.println(equipoUsuario.get(turnoUsuario).getNombre() + " ha sido derrotado");
+                    equipoUsuario.remove(turnoUsuario);
 
-    public Entrenador getEntrenador() {
-        return entrenador;
+                    if (equipoUsuario.isEmpty()){
+                        System.out.println("Has perdido el combate");
+                        return;
+                    }
+                }
+            } else {
+                System.out.println("Seleccion no valida");
+            }
+        }
     }
-
-    public void setEntrenador(Entrenador entrenador) {
-        this.entrenador = entrenador;
+    
+    private void realizarAtaque(Habilidades habilidad, PokemonCombate objetivo){
+        int daño = habilidad.getPotencia();
+        objetivo.setHp_base(objetivo.getHp_base() - daño);
+        System.out.println(objetivo.getNombre() + " ha recibido " + daño + " puntos de daño");
     }
-
-    public Entrenador getContrincante() {
-        return contrincante;
-    }
-
-    public void setContrincante(Entrenador contrincante) {
-        this.contrincante = contrincante;
-    }
-
-    public String getTurnoActual() {
-        return turnoActual;
-    }
-
-    public void setTurnoActual(String turnoActual) {
-        this.turnoActual = turnoActual;
+    
+    private void realizarAtaqueComputadora (PokemonCombate atacante, PokemonCombate objetivo) {
+        int indiceHabilidad = (int) (Math.random() * atacante.getHabilidades().size());
+        Habilidades habilidadSeleccionada = atacante.getHabilidades().get(indiceHabilidad);
+        
+        System.out.println("La computadora utiliza " + habilidadSeleccionada.getNombreHabilidad() + " con potencia " + habilidadSeleccionada.getPotencia());
+        
+        realizarAtaque(habilidadSeleccionada, objetivo);
     }
 }
--- src/main/java/com/mycompany/proyecto_final/Proyecto_Final.java
+++ src/main/java/com/mycompany/proyecto_final/Proyecto_Final.java
@@ -1,121 +1,169 @@
 package com.mycompany.proyecto_final;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Proyecto_Final {
 
+    private Scanner scanner = new Scanner(System.in);
+    private boolean salir = false;
+    private ArrayList<PokemonCombate> equipoUsuario = new ArrayList<>();
+    private ArrayList<PokemonCombate> equipoComputadora = new ArrayList<>();
+
     public static void main(String[] args) {
-        
         ConexionDB conec = new ConexionDB();
         conec.estableceConexion();
-        
-        Proyecto_Final proyecto = new Proyecto_Final();
-        proyecto.MenuPokemon(); 
-    }
-        Scanner scanner = new Scanner(System.in);
-        boolean salir = false;       
 
-//Menu principal       
-    public void MenuPokemon(){
-        System.out.println("Ingrese una opción: ");
+        Proyecto_Final proyecto = new Proyecto_Final();
+        proyecto.MenuPokemon();
+    }
+
+    // Menu principal
+    public void MenuPokemon() {
+        while (!salir) {
+            System.out.println("Ingrese una opción: ");
             System.out.println("1. Ver Pokedex");
             System.out.println("2. Combate");
-            
-        int opcion1 = scanner.nextInt();
-        scanner.nextLine();
-        
-        switch (opcion1){
-            case 1: 
-                menuPokedex();
-                break;
-            case 2:
-                menuPrincipalCombate();
-                break;
+
+            int opcion1 = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion1) {
+                case 1:
+                    menuPokedex();
+                    break;
+                case 2:
+                    menuPrincipalCombate();
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
         }
     }
-//Menu para la Pokedex        
-    public void menuPokedex(){
+
+    // Menu para la Pokedex
+    public void menuPokedex() {
         while (!salir) {
-              System.out.println("Ingrese una opción:");
-                System.out.println("1. Ver pokemones normales");
-                System.out.println("2. Ver pokemones de Agua");
-                System.out.println("3. Ver pokemones de Fuego");
-                System.out.println("4. Ver pokemones de Planta");
-                System.out.println("5. Ver pokemones eléctricos");
-                System.out.println("6. Ver pokemones de Hielo");
-                System.out.println("7. Mostrar todos");
-                System.out.println("8. Salir");
+            System.out.println("Ingrese una opción:");
+            System.out.println("1. Ver pokemones normales");
+            System.out.println("2. Ver pokemones de Agua");
+            System.out.println("3. Ver pokemones de Fuego");
+            System.out.println("4. Ver pokemones de Planta");
+            System.out.println("5. Ver pokemones eléctricos");
+            System.out.println("6. Ver pokemones de Hielo");
+            System.out.println("7. Mostrar todos");
+            System.out.println("8. Salir");
 
             int opcion = scanner.nextInt();
             scanner.nextLine();
 
             TipoPokemon tipoPokemon = new TipoPokemon();
             Pokedex pokedex = new Pokedex();
-            
+
             switch (opcion) {
                 case 1:
                     tipoPokemon.getPokemonesNormales();
                     break;
-                    
                 case 2:
-                 tipoPokemon.getPokemonesDeAgua();
+                    tipoPokemon.getPokemonesDeAgua();
                     break;
-
                 case 3:
-                   tipoPokemon.getPokemonesDeFuego();
-
+                    tipoPokemon.getPokemonesDeFuego();
                     break;
-
                 case 4:
-                     tipoPokemon.getPokemonesDePlanta();
+                    tipoPokemon.getPokemonesDePlanta();
                     break;
-                    
-                   case 5:
-                     tipoPokemon.getPokemonesDeElectrico();
+                case 5:
+                    tipoPokemon.getPokemonesDeElectrico();
                     break;
-                    
-                 case 6:
-                     tipoPokemon.getPokemonesDeHielo();
+                case 6:
+                    tipoPokemon.getPokemonesDeHielo();
                     break;
-                    
-                 case 7:
-                     pokedex.getTodosLosPokemon();
-                     break;
-                    
-                   case 8:
-                   salir = true;
+                case 7:
+                    pokedex.getTodosLosPokemon();
                     break;
-
+                case 8:
+                    salir = true;
+                    break;
                 default:
                     System.out.println("Opción no válida. Por favor intente de nuevo.");
             }
         }
         System.out.println("Programa terminado.");
     }
-    
-    public void menuPrincipalCombate(){
+
+    public void menuPrincipalCombate() {
         System.out.println("Ingrese una opción:");
-            System.out.println("1. Ver Pokemon disponibles para combate.");
-            
+        System.out.println("1. Ver Pokémon disponibles para combate.");
+
         int opcion1 = scanner.nextInt();
         scanner.nextLine();
-        
-        switch (opcion1){
-            case 1: 
+
+        switch (opcion1) {
+            case 1:
                 menuPokemonDisponiblesCombate();
                 break;
+            default:
+                System.out.println("Opción no válida.");
         }
     }
-    
-    public void menuPokemonDisponiblesCombate(){
-        System.out.println("1. Ver Pokemon tipo agua.");
-        
-        int opcion1 = scanner.nextInt();
+
+    public void menuPokemonDisponiblesCombate() {
+        System.out.println("1. Ver Pokémon tipo agua.");
+        System.out.println("8. Regresar a menú principal");
+
+        int opcion = scanner.nextInt();
         scanner.nextLine();
-        
-        switch (opcion1){
-            case 1: 
-                PokemonCombateAgua.pokemonCombate_Agua(null);
+
+        switch (opcion) {
+            case 1:
+                seleccionarPokemonAgua(equipoUsuario, scanner);
+                break;
+            case 8:
+                MenuPokemon();
+                break;
+            default:
+                System.out.println("Opción no válida.");
         }
+    }
+
+    private void seleccionarPokemonAgua(ArrayList<PokemonCombate> equipoUsuario, Scanner scanner) {
+        ArrayList<PokemonCombate> pokemonAgua = PokemonCombateAgua.obtenerPokemonAgua();
+
+        while(equipoUsuario.size() < 3){
+            System.out.println("Seleccione un Pokémon tipo agua:");
+            for (int i = 0; i < pokemonAgua.size(); i++) {
+                System.out.println((i + 1) + " " + pokemonAgua.get(i).getNombre());
+            }
+
+            int seleccion = scanner.nextInt();
+            scanner.nextLine();
+
+            if (seleccion > 0 && seleccion <= pokemonAgua.size()) {
+                PokemonCombate pokemonSeleccionado = pokemonAgua.get(seleccion - 1);
+                equipoUsuario.add(pokemonSeleccionado);
+                System.out.println("Has seleccionado a " + pokemonSeleccionado.getNombre());
+            } else {
+                System.out.println("Selección no válida.");
+            }
+        }
+        iniciarCombate();
+    }
+
+    private void seleccionarEquipoComputadora(ArrayList<PokemonCombate> equipoComputadora, ArrayList<PokemonCombate> equipoUsuario) {
+        ArrayList<PokemonCombate> pokemonDisponibles = PokemonCombateAgua.obtenerPokemonAgua();
+
+        pokemonDisponibles.removeAll(equipoUsuario);
+
+        while (equipoComputadora.size() < 3) {
+            int indiceAleatorio = (int) (Math.random() * pokemonDisponibles.size());
+            equipoComputadora.add(pokemonDisponibles.remove(indiceAleatorio));
+        }
+        System.out.println("La computadora ha seleccionado a sus Pokémon.");
+    }
+
+    private void iniciarCombate() {
+        seleccionarEquipoComputadora(equipoComputadora, equipoUsuario);
+        Combate combate = new Combate(equipoUsuario, equipoComputadora);
+        combate.iniciarCombate(scanner);
     }
 }
